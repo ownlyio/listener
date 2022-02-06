b2a21dbbd3daf5556b520fff03681eda14f3fa52
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2022-02-06 22:08:11 +0800

    Add chain id for testnet

--- scripts/marketplace.js
+++ scripts/marketplace.js
@@ -23,72 +23,78 @@ const marketplaceBsc = new web3Bsc.eth.Contract(
 
 let fromBlock = 0;
 web3Eth.eth.getBlockNumber().then((data) => {
-  // fromBlock = data;
+    // fromBlock = data;
 
-  marketplaceEth.events.MarketItemCreated(
-      {fromBlock: fromBlock, step: 0}
-  ).on('data', async event => {
-    let chain_id = 1;
+    marketplaceEth.events.MarketItemCreated(
+        {fromBlock: fromBlock, step: 0}
+    ).on('data', async event => {
+        let chain_id = 4;
 
-    console.log('\nMarket Item Created');
+        console.log('\nMarket Item Created');
 
-    console.log("\nevent.returnValues:");
-    console.log(event.returnValues);
+        console.log("\nevent.returnValues:");
+        console.log(event.returnValues);
 
-    marketplaceBsc.methods.getMessageHash(chain_id, event.returnValues[0], event.returnValues[1], event.returnValues[2], event.returnValues[3], event.returnValues[4], event.returnValues[5], event.returnValues[6]).call()
-      .then(function(messageHash) {
-        console.log("\nmessageHash:");
-        console.log(messageHash);
+        marketplaceBsc.methods.getMessageHash(chain_id, event.returnValues[0], event.returnValues[1], event.returnValues[2], event.returnValues[3], event.returnValues[4], event.returnValues[5], event.returnValues[6]).call()
+            .then(function(messageHash) {
+                console.log("\nmessageHash:");
+                console.log(messageHash);
 
-        let signatureObject = web3Eth.eth.accounts.sign(messageHash, process.env.MARKETPLACE_VALIDATOR_PRIVATE_KEY);
+                let signatureObject = web3Eth.eth.accounts.sign(messageHash, process.env.MARKETPLACE_VALIDATOR_PRIVATE_KEY);
 
-        console.log("\nsignature:");
-        console.log(signatureObject.signature);
+                console.log("\nsignature:");
+                console.log(signatureObject.signature);
 
-        marketplaceBsc.methods.verify(chain_id, event.returnValues[0], event.returnValues[1], event.returnValues[2], event.returnValues[3], event.returnValues[4], event.returnValues[5], event.returnValues[6], signatureObject.signature).call()
-            .then(function(verify) {
-              console.log("\nverify:");
-              console.log(verify);
+                marketplaceBsc.methods.verify(chain_id, event.returnValues[0], event.returnValues[1], event.returnValues[2], event.returnValues[3], event.returnValues[4], event.returnValues[5], event.returnValues[6], signatureObject.signature).call()
+                    .then(function(verify) {
+                        console.log("\nverify:");
+                        console.log(verify);
 
-              axios.post("https://ownly.tk/api/store-market-item", {
-                chain_id: chain_id,
-                item_id: event.returnValues[0],
-                message_hash: messageHash,
-                signature: signatureObject.signature,
-                event: JSON.stringify(event)
-              }).then(data => {
-                console.log("\nstore-market-item:");
-                console.log(data.data);
-              }).catch(function(error) {
-                console.log(error)
-              });
+                        axios.post("https://ownly.tk/api/store-market-item", {
+                            chain_id: chain_id,
+                            item_id: event.returnValues[0],
+                            message_hash: messageHash,
+                            signature: signatureObject.signature,
+                            event: JSON.stringify(event)
+                        }).then(data => {
+                            console.log("\nstore-market-item:");
+                            console.log(data.data);
+                        }).catch(function(error) {
+                            console.log(error)
+                        });
+                    });
             });
-      });
 
-    // const { from, to, amount, date, nonce, signature } = event.returnValues;
-    //
-    // const tx = bridgeBsc.methods.mint(from, to, amount, nonce, signature);
-    // const [gasPrice, gasCost] = await Promise.all([
-    //   web3Bsc.eth.getGasPrice(),
-    //   tx.estimateGas({from: admin}),
-    // ]);
-    // const data = tx.encodeABI();
-    // const txData = {
-    //   from: admin,
-    //   to: bridgeBsc.options.address,
-    //   data,
-    //   gas: gasCost,
-    //   gasPrice
-    // };
-    // const receipt = await web3Bsc.eth.sendTransaction(txData);
-  });
+        // const { from, to, amount, date, nonce, signature } = event.returnValues;
+        //
+        // const tx = bridgeBsc.methods.mint(from, to, amount, nonce, signature);
+        // const [gasPrice, gasCost] = await Promise.all([
+        //   web3Bsc.eth.getGasPrice(),
+        //   tx.estimateGas({from: admin}),
+        // ]);
+        // const data = tx.encodeABI();
+        // const txData = {
+        //   from: admin,
+        //   to: bridgeBsc.options.address,
+        //   data,
+        //   gas: gasCost,
+        //   gasPrice
+        // };
+        // const receipt = await web3Bsc.eth.sendTransaction(txData);
+    });
 
-  // marketplaceEth.events.MarketItemCancelled(
-  //     {fromBlock: fromBlock, step: 0}
-  // ).on('data', async event => {
-  //   console.log('\nMarket Item Cancelled');
-  // });
+    marketplaceEth.events.MarketItemPaidForOtherChain(
+        {fromBlock: fromBlock, step: 0}
+    ).on('data', async event => {
 
-  console.log('Latest Block: ' + fromBlock);
-  console.log('Running...');
+    });
+
+    // marketplaceEth.events.MarketItemCancelled(
+    //     {fromBlock: fromBlock, step: 0}
+    // ).on('data', async event => {
+    //   console.log('\nMarket Item Cancelled');
+    // });
+
+    console.log('Latest Block: ' + fromBlock);
+    console.log('Running...');
 });
