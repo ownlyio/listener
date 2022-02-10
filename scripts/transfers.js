0263837a42f5a1560b85f41876c313887888f1ba
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2022-02-10 23:59:37 +0800

    Create listener for token transfers

--- scripts/transfers.js
+++ scripts/transfers.js
@@ -1,56 +1,113 @@
 const Web3 = require('web3');
 const axios = require('axios');
+const ethers = require('ethers');
 
 require('dotenv').config();
 
-const web3Eth = new Web3("wss://eth-mainnet.alchemyapi.io/v2/n60TiXLxyxLjDkrPnw_mzx4GahkWWkSN");
-const web3Bsc = new Web3(process.env.RPC_URL_BSC);
-const web3Matic = new Web3(process.env.RPC_URL_MATIC);
+let web3Eth = new Web3(process.env.RPC_URL_ETH);
+web3Eth = new Web3("wss://eth-mainnet.alchemyapi.io/v2/n60TiXLxyxLjDkrPnw_mzx4GahkWWkSN");
+let web3Bsc = new Web3(process.env.RPC_URL_BSC);
+web3Bsc = new Web3("wss://apis.ankr.com/wss/0407faa2327343cdaab0119f931d0774/3fbdcb07f9c77acb75faaa0ac3cae996/binance/full/main");
+let web3Matic = new Web3(process.env.RPC_URL_MATIC);
+
+const webSocketProvider = new ethers.providers.WebSocketProvider("wss://speedy-nodes-nyc.moralis.io/66aa4c60304ba7f399f9eedd/bsc/mainnet/ws");
 
 axios.get(process.env.OWNLY_URL + "/api/get-collections").then(data => {
     let collections = data.data.collections;
-
     let collectionContracts = [];
 
     for(let i = 0; i < collections.length; i++) {
+        let processBlockchainData = function(collection, event, currency) {
+            axios.get("https://api.covalenthq.com/v1/" + collections[i].chain_id + "/transaction_v2/" + event.transactionHash + "/?quote-currency=USD&format=JSON&no-logs=false&key=ckey_994c8fdd549f44fa9b2b27f59a0").then(data => {
+                let value = data.data.items[0].value;
+
+                axios.post(process.env.OWNLY_URL + "/api/store-token-transaction", {
+                    chain_id: collections[i].chain_id,
+                    contract_address: collections[i].contract_address,
+                    token_id: event.returnValues.tokenId,
+                    from: event.returnValues.from,
+                    to: event.returnValues.to,
+                    value: value,
+                    currency: currency,
+                    transaction_hash: event.transactionHash,
+                    signed_at: data.data.items[0].block_signed_at,
+                }).then(data => {
+                    console.log("\nstore-market-item:");
+                    console.log(data.data);
+                }).catch(function (error) {
+                    console.log(error)
+                });
+            }).catch(function (error) {
+                console.log(error)
+            });
+        };
+
         if (collections[i].chain_id === 1) {
-            collectionContracts[i] = new web3Eth.eth.Contract(
-                JSON.parse(collections[i].abi),
-                collections[i].contract_address
-            );
+            // collectionContracts[i] = new web3Eth.eth.Contract(
+            //     JSON.parse(collections[i].abi),
+            //     collections[i].contract_address
+            // );
+            //
+            // collectionContracts[i].getPastEvents('Transfer', {
+            //     fromBlock: 0,
+            //     toBlock: 'latest'
+            // }).then(results => console.log(results))
+            //     .catch(err => {
+            //
+            // });
 
-            web3Eth.eth.getBlockNumber().then((data) => {
-                // let fromBlock = data;
-                let fromBlock = 0;
+            // web3Eth.eth.getBlockNumber().then((data) => {
+            //     // let fromBlock = data;
+            //     let fromBlock = 0;
+            //
+            //     console.log("\nContract Address");
+            //     console.log(collectionContracts[i].events);
+            //
+            //     collectionContracts[i].events.Transfer(
+            //         {fromBlock: fromBlock, step: 0}
+            //     ).on('data', async event => {
+            //         console.log(event.returnValues);
+            //
+            //         // processBlockchainData(collections[i], event, "ETH");
+            //     });
+            // });
+        } else if(collections[i].chain_id === 56) {
+            const contract = new ethers.Contract(collections[i].contract_address, JSON.parse(collections[i].abi), webSocketProvider)
 
-                console.log("\nContract Address");
-                console.log(collectionContracts[i].events);
-
-                collectionContracts[i].events.Transfer(
-                    {fromBlock: fromBlock, step: 0}
-                ).on('data', async event => {
-                    console.log('\nTransfer');
-
-                    console.log("\nevent.returnValues:");
-                    console.log(event);
+            contract.on("Transfer", (from, to, value, event) => {
+                console.log({
+                    from: from,
+                    to: to,
+                    value: value.toNumber(),
+                    data: event
                 });
             });
+
+            // collectionContracts[i] = new web3Bsc.eth.Contract(
+            //     JSON.parse(collections[i].abi),
+            //     collections[i].contract_address
+            // );
+            //
+            // console.log("\nContract Address");
+            // console.log(collections[i].contract_address);
+            //
+            // web3Bsc.eth.getBlockNumber().then((data) => {
+            //     // let fromBlock = data;
+            //     let fromBlock = 0;
+            //
+            //     console.log(fromBlock);
+            //
+            //     collectionContracts[i].events.Transfer(
+            //         {fromBlock: fromBlock, step: 0}
+            //     ).on('data', async event => {
+            //         console.log(event);
+            //         // processBlockchainData(collections[i], event, "BNB");
+            //     });
+            // });
+        } else if(collections[i].chain_id === 136) {
+
         }
     }
 }).catch(function(error) {
     console.log(error)
 });
-
-// let collectionContract = new web3Eth.eth.Contract(
-//     [{"inputs":[{"internalType":"address","name":"_proxyRegistryAddress","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"baseTokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"contractURI","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isOwner","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_to","type":"address"}],"name":"mintTo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}],
-//     "0x804efc52BFa9B85A86219c191d51147566f3B327"
-// );
-//
-// collectionContract.events.Transfer(
-//     {fromBlock: 0, step: 0}
-// ).on('data', async event => {
-//     console.log('\nTransfer');
-//
-//     console.log("\nevent.returnValues:");
-//     console.log(event.returnValues);
-// });
